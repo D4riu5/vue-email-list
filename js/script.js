90cf05f86735ec05dac424b317c4a317f3f207ea
D4riu5 <R3iji94@gmail.com>
2023-01-31 13:57:20 +0100

    V 1: - (Basic) emails received with the api get added into an array, using axios and a for loop - (Bonus) if array length == 10, print emails on page

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@
   createApp({
     data() {
       return {
-
+        emails: [],
+        apiLoaded: false,
       }
     },
     methods:{
@@ -12,6 +13,20 @@
     },
     mounted() {
     console.log(`the component is now mounted.`)
+
+    for (let i = 1; i <= 10; i++) {
+      axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(
+          resp => {
+
+            data = resp.data.response;
+            this.emails.push(data)
+            if (this.emails.length == 10) {
+              this.apiLoaded = true;
+            }
+          });
+      }
   }
   }).mount('#app')
 
